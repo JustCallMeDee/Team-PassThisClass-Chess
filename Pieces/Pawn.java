e8d0e666655eeeb845be04b53a7458ef1489bd41
Dee Quinn <ofo21@txstate.edu>
2026-03-17 20:43:52 -0500

    Partial implementation of Pawn move function

--- Pieces/Pawn.java
+++ Pieces/Pawn.java
@@ -15,7 +15,7 @@ public class Pawn extends Piece{
     public boolean getHasMoved(){
         return hasMoved;
     }
-    
+
     @Override
     public boolean move(Position newPos){
         boolean successful = super.move(newPos);
@@ -24,10 +24,65 @@ public class Pawn extends Piece{
         }
         return successful;
     }
+    
     @Override
     public ArrayList<Position> possibleMoves() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'possibleMoves'");
+        ArrayList<Position> positions = new ArrayList<Position>();
+
+        //Check moving spaces first
+        //+1/2 if white, -1/2 if black
+        Position check;
+        try{
+            check = new Position(this.getPosition().getRow() + 1, this.getPosition().getColumn());
+            /*
+            Unimplemented:
+                Check that this will not land on the same space as a same-color piece
+                or that it will leave the king in check. Likely leave this to the 
+                Board class.
+            */
+            positions.add(check);
+        }
+        catch (IllegalArgumentException e){
+            //yup
+        }
+
+        if(!hasMoved){
+            try{
+                check = new Position(this.getPosition().getRow() + 2, this.getPosition().getColumn());
+                /*
+                Unimplemented:
+                    Check that this will not land on the same space as a same-color piece
+                    or that it will leave the king in check. Likely leave this to the 
+                    Board class.
+                */
+                positions.add(check);
+            }
+            catch (IllegalArgumentException e){
+                //yup
+            }
+        }
+
+        //Now check if there is opposing pieces to the front diagonals
+        //If so, add those
+        //Unimplemented: Needs board to be done
+        //DEBUG STATEMENTS
+        boolean pieceLeft = true, pieceRight = false; 
+
+        //Since a piece already occupies the spaces we are checking, only need to check if king is in check
+        if(pieceLeft){
+            check = new Position(this.getPosition().getRow() + 1, this.getPosition().getColumn() - 1);
+            //Check if check
+            positions.add(check);
+        }
+
+        if(pieceRight){
+            check = new Position(this.getPosition().getRow() + 1, this.getPosition().getColumn() + 1);
+            //Check if check
+            positions.add(check);
+        }
+
+
+        return positions;
     }
     
 }
