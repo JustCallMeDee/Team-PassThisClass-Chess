b3d924411aa0cc2f20fa4968271151ace8d51840
mattd1az <mattdylandiaz@gmail.com>
2026-03-24 00:01:15 -0500

    Update Board.java
    reworked code to account for pieces being taken and updating board

--- Board/Board.java
+++ Board/Board.java
@@ -1,146 +1,128 @@
-package Board;
+package board;
 
-import Pieces.*;
-import Pieces.Piece;
-import Player.*;
+import pieces.*;
+import player.*;
+import java.util.*;
 
 public class Board {
-    private Position[][] board;
+    private Piece[][] board;
     private Player whitePlayer;
     private Player blackPlayer;
-    public void initializeBoard() {
-
-    for (int col = 0; col < 8; col++) {
-        whitePlayer.addPiece(new Pawn(Color.White, new Position(6, col)));
-        blackPlayer.addPiece(new Pawn(Color.Black, new Position(1, col)));
-    }
-
-    whitePlayer.addPiece(new Rook(Color.White, new Position(7, 0)));
-    whitePlayer.addPiece(new Rook(Color.White, new Position(7, 7)));
-    blackPlayer.addPiece(new Rook(Color.Black, new Position(0, 0)));
-    blackPlayer.addPiece(new Rook(Color.Black, new Position(0, 7)));
-
-    whitePlayer.addPiece(new Knight(Color.White, new Position(7, 1)));
-    whitePlayer.addPiece(new Knight(Color.White, new Position(7, 6)));
-    blackPlayer.addPiece(new Knight(Color.Black, new Position(0, 1)));
-    blackPlayer.addPiece(new Knight(Color.Black, new Position(0, 6)));
-
-    whitePlayer.addPiece(new Bishop(Color.White, new Position(7, 2)));
-    whitePlayer.addPiece(new Bishop(Color.White, new Position(7, 5)));
-    blackPlayer.addPiece(new Bishop(Color.Black, new Position(0, 2)));
-    blackPlayer.addPiece(new Bishop(Color.Black, new Position(0, 5)));
-
-    whitePlayer.addPiece(new Queen(Color.White, new Position(7, 3)));
-    blackPlayer.addPiece(new Queen(Color.Black, new Position(0, 3)));
-
-    whitePlayer.addPiece(new King(Color.White, new Position(7, 4)));
-    blackPlayer.addPiece(new King(Color.Black, new Position(0, 4)));
-}
 
     public Board(Player whitePlayer, Player blackPlayer) {
         this.whitePlayer = whitePlayer;
         this.blackPlayer = blackPlayer;
-        this.board = new Position[8][8];
-        for(int i=0; i<8; i++){
-            for(int j=0; j<8; j++){
-                board[i][j] = new Position(i, j);
-            }
-        }
-    }
-    public Board(){
-        this(new Player(Color.White), new Player(Color.Black));
-        //initializeBoard();
+        this.board = new Piece[8][8];
     }
 
-    public Player getPlayer(Color color){
-        if(color == Color.White){
-            return getWhitePlayer();
-        }
-        else{
-            return getBlackPlayer();
-        }
+    public Board() {
+        this(new Player(Color.WHITE), new Player(Color.BLACK));
+        initializeBoard();
     }
 
-    public Player getWhitePlayer(){
+    public void initializeBoard() {
+        for (int col = 0; col < 8; col++) {
+            Pawn whitePawn = new Pawn(Color.WHITE, new Position(6, col));
+            Pawn blackPawn = new Pawn(Color.BLACK, new Position(1, col));
+            placeAndAdd(whitePawn, whitePlayer);
+            placeAndAdd(blackPawn, blackPlayer);
+        }
+
+        placeAndAdd(new Rook(Color.WHITE, new Position(7, 0)), whitePlayer);
+        placeAndAdd(new Rook(Color.WHITE, new Position(7, 7)), whitePlayer);
+        placeAndAdd(new Rook(Color.BLACK, new Position(0, 0)), blackPlayer);
+        placeAndAdd(new Rook(Color.BLACK, new Position(0, 7)), blackPlayer);
+
+        placeAndAdd(new Knight(Color.WHITE, new Position(7, 1)), whitePlayer);
+        placeAndAdd(new Knight(Color.WHITE, new Position(7, 6)), whitePlayer);
+        placeAndAdd(new Knight(Color.BLACK, new Position(0, 1)), blackPlayer);
+        placeAndAdd(new Knight(Color.BLACK, new Position(0, 6)), blackPlayer);
+
+        placeAndAdd(new Bishop(Color.WHITE, new Position(7, 2)), whitePlayer);
+        placeAndAdd(new Bishop(Color.WHITE, new Position(7, 5)), whitePlayer);
+        placeAndAdd(new Bishop(Color.BLACK, new Position(0, 2)), blackPlayer);
+        placeAndAdd(new Bishop(Color.BLACK, new Position(0, 5)), blackPlayer);
+
+        placeAndAdd(new Queen(Color.WHITE, new Position(7, 3)), whitePlayer);
+        placeAndAdd(new Queen(Color.BLACK, new Position(0, 3)), blackPlayer);
+
+        placeAndAdd(new King(Color.WHITE, new Position(7, 4)), whitePlayer);
+        placeAndAdd(new King(Color.BLACK, new Position(0, 4)), blackPlayer);
+    }
+
+    private void placePiece(Piece piece) {
+        Position pos = piece.getPosition();
+        board[pos.getRow()][pos.getCol()] = piece;
+    }
+
+    private void placeAndAdd(Piece piece, Player player) {
+        placePiece(piece);
+        piece.setPosition(piece.getPosition());
+        player.addPiece(piece);
+    }
+
+    public Player getPlayer(Color color) {
+        return (color == Color.WHITE) ? whitePlayer : blackPlayer;
+    }
+
+    public Player getWhitePlayer() {
         return whitePlayer;
     }
 
-    public Player getBlackPlayer(){
+    public Player getBlackPlayer() {
         return blackPlayer;
     }
 
-    public Piece getPiece(Position pos){
-        Piece p = whitePlayer.findPieceAt(pos);
-        if(p == null){
-            p = blackPlayer.findPieceAt(pos);
-        }
-        return p;
+    public Piece getPiece(Position pos) {
+        return board[pos.getRow()][pos.getCol()];
     }
 
-   public boolean movePiece(Position start, Position end){
-      Piece piece = getPiece(start);
-      
-      if(piece == null){
-            return false;
-        }
+    public boolean movePiece(Position start, Position end) {
+        Piece piece = getPiece(start);
+        if (piece == null) return false;
 
         Piece target = getPiece(end);
+        if (target != null && target.getColor() == piece.getColor()) return false;
 
-    if(piece.getColor() == Color.White){
-        whitePlayer.makeMove(start, end);
-        if(target != null && target.getColor() == Color.Black){
-            blackPlayer.capturePiece(end);
+        board[start.getRow()][start.getCol()] = null;
+
+        if (target != null) {
+            if (target.getColor() == Color.WHITE) whitePlayer.capturePiece(end);
+            else blackPlayer.capturePiece(end);
         }
-    } else {
-        blackPlayer.makeMove(start, end);
-        if(target != null && target.getColor() == Color.White){
-            whitePlayer.capturePiece(end);
-        }
+
+        piece.setPosition(end);
+        board[end.getRow()][end.getCol()] = piece;
+
+        if (piece.getColor() == Color.WHITE) whitePlayer.makeMove(start, end);
+        else blackPlayer.makeMove(start, end);
+
+        return true;
     }
 
-    return true;
-}
-
-    public boolean isCheck(){
+    public boolean isCheck(Color color) {
         return false;
     }
 
-    public boolean isCheck(Position moveStart, Position moveEnd){
+    public boolean isCheck(Position moveStart, Position moveEnd) {
         return false;
     }
 
-    public boolean isCheckmate(){
-        if(!isCheck()){
-            return false;
-        }
-        else{
-            return false;
-        }
+    public boolean isCheckmate(Color color) {
+        return false;
     }
 
-    @Override
-    public String toString(){
-        StringBuilder sb = new StringBuilder();
-
-    sb.append("  A  B  C  D  E  F  G  H\n");
-
-    for (int row = 0; row < 8; row++) {
-        int displayRow = 8 - row;
-        sb.append(displayRow + " ");
-
-        for (int col = 0; col < 8; col++) {
-            Position pos = board[row][col];
-            Piece piece = getPiece(pos);
-
-            if (piece != null) {
-                sb.append(piece.toString() + " ");
-            } else {
-                sb.append("## ");
+    public void display() {
+        System.out.println("  A  B  C  D  E  F  G  H");
+        for (int row = 0; row < 8; row++) {
+            int displayRow = 8 - row;
+            System.out.print(displayRow + " ");
+            for (int col = 0; col < 8; col++) {
+                Piece piece = board[row][col];
+                if (piece != null) System.out.print(piece.toString() + " ");
+                else System.out.print("## ");
             }
+            System.out.println();
         }
-        sb.append("\n");
     }
-
-    return sb.toString();
-}
 }
