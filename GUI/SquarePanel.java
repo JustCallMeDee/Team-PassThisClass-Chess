c3a3875f6bb0390c50d6b17801eb49cb51bb7b9f
Dee Quinn <ofo21@txstate.edu>
2026-04-15 17:49:37 -0500

    Pieces are now images

--- GUI/SquarePanel.java
+++ GUI/SquarePanel.java
@@ -7,12 +7,20 @@ import Pieces.Piece;
 public class SquarePanel extends JButton {
 
     private int row, col;
+    private JLabel imageLabel;
+    private final String PATH_TO_PIECE_VISUALS = "C:\\Users\\dalto\\OneDrive\\Desktop\\Programs\\Java\\Chess\\GUI\\PieceVisual\\";
+    private String colorPath;
+    private int size = 75;
 
     public SquarePanel(int row, int col, Color color){
         this.row = row;
         this.col = col;
         setBackground(color);
-        setFont(new Font("Arial", Font.BOLD, 18));
+        imageLabel = new JLabel();
+        imageLabel.setIcon(new ImageIcon());
+        this.add(imageLabel);
+        //setFont(new Font("Arial", Font.BOLD, 18));
+        colorPath = "Black\\";
     }
 
     public int getRow() {return row;}
@@ -20,9 +28,33 @@ public class SquarePanel extends JButton {
 
     public void setPiece(Piece piece){
         if (piece == null) {
-            setText("");
+            imageLabel.setIcon(new ImageIcon());
         } else {
-            setText(piece.toString());
+            //Switch case to set correct path
+            String fullPath = PATH_TO_PIECE_VISUALS + colorPath;
+            switch (piece.toString().charAt(1)) {
+                case 'P':
+                    fullPath += "pawn.png\\";
+                    break;
+                case 'N':
+                    fullPath += "knight.png\\";
+                    break;
+                case 'B':
+                    fullPath += "bishop.png\\";
+                    break;
+                case 'R':
+                    fullPath += "rook.png\\";
+                    break;
+                case 'Q':
+                    fullPath += "queen.png\\";
+                    break;
+                case 'K':
+                    fullPath += "king.png\\";
+                    break;
+                default:
+                    break;
+            }
+            imageLabel.setIcon(new ImageIcon(new ImageIcon(fullPath).getImage().getScaledInstance(size, size, Image.SCALE_AREA_AVERAGING)));
         }
     }
 }
